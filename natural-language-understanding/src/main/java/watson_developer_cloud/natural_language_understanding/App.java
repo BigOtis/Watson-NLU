ff5f4a2f3f58e08c6dec616d9f46119e88560c55
Phillip Lopez <pgl5711@rit.edu>
2017-03-15 21:43:01 -0400

    Created initial maven project

---
+++ natural-language-understanding/src/main/java/watson_developer_cloud/natural_language_understanding/App.java
@@ -0,0 +1,13 @@
+package watson_developer_cloud.natural_language_understanding;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
